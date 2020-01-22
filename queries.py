c3dc6b2a21d8b5be2a8d841a63ad43d15f95ebba
Rocky <rockyspiker@gmail.com>
2020-01-21 22:28:28 -0700

    Added listing all cities with their suppliers

--- queries.py
+++ queries.py
@@ -8,7 +8,7 @@ boltSupps = {supp[1] for supp in parts.suppliers
 
 print('"Bolt" suppliers are listed as:')
 for supp in boltSupps:
-  print(supp)
+  print('\t' + supp)
 
 # part two
 redSupps = {supp[1] for supp in parts.suppliers
@@ -18,16 +18,26 @@ redSupps = {supp[1] for supp in parts.suppliers
 
 print('\n"Red" suppliers are listed as:')
 for supp in redSupps:
-  print(supp)
+  print('\t' + supp)
 
 # part three
-redSupps = {supp[1] for supp in parts.suppliers
-                       for r in parts.spj
-                       for part in parts.parts 
-                       if r[1]==part[0] if r[0]==supp[0] if part[2]=='Red'}
+pairCity = {city[3] for city in parts.suppliers
+                        for supp in parts.suppliers
+                        if city[1]!=supp[1] if city[3]==supp[3]
+}
 
-print('\n"Red" suppliers are listed as:')
-for supp in redSupps:
-  print(supp)
+print('\nCity with pairs list as:')
+for city in pairCity:
+  print('\t' + city + ' suppliers:')
+  for supp in parts.suppliers:
+    if supp[3] == city:
+      print('\t\t' + supp[1])
 
 #part four
+cities = {city[3] for city in parts.suppliers}
+print('\nCities with supplies list as:')
+for city in cities:
+  print('\t' + city + ' suppliers:')
+  for supp in parts.suppliers:
+    if supp[3] == city:
+      print('\t\t' + supp[1])
